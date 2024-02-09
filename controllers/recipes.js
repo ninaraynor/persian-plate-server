dd26e43459a08e58daf92c07b4ae1add23362e22
ninaraynor <ninaraynor@gmail.com>
2024-02-09 11:09:28 -0500

    Add sortby function to index

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -3,13 +3,34 @@ const {Recipe} = require('../models')
 
 
 // Recipe index action
-const index = async (req,res,next) => {
-	try {
-    res.json(await Recipe.find({}));
+const index = async (req, res, next) => {
+  try {
+    const sortBy = req.query.sortBy || 'title';
+    const sortOrder = req.query.sortOrder || 'asc';
+    let recipes = await Recipe.find({}).sort({ [sortBy]: sortOrder });
+    res.json(recipes);
   } catch (error) {
     res.status(400).json(error);
   }
 }
+
+// const index = async (req, res, next) => {
+//   try {
+//     const sortBy = req.query.sortBy || 'name';
+//     let sortOrder = req.query.sortOrder || 'asc';
+//     if (sortBy === 'date') {
+//       sortOrder = sortOrder === 'desc' ? -1 : 1;
+//     }
+//     if (sortBy === 'name') {
+//       let recipes = await Recipe.find({}).sort({ name: sortOrder });
+//       return res.json(recipes);
+//     }
+//     let recipes = await Recipe.find({}).sort({ [sortBy]: sortOrder });
+//     res.json(recipes);
+//   } catch (error) {
+//     res.status(400).json(error);
+//   }
+// }
 
 // Recipe create
 const create = async (req,res,next) => {
