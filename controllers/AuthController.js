74b31eaa0f4e801bcabc375d1e86fab0a3445bf5
ninaraynor <ninaraynor@gmail.com>
2024-02-13 10:39:10 -0500

    add check session

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -43,7 +43,13 @@ console.log(User)
   }
 }
 
+const CheckSession = async (req, res) => {
+  const { payload } = res.locals
+  res.send(payload)
+}
+
 module.exports = {
   Login,
-  Register
+  Register,
+  CheckSession
 }
--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -3,5 +3,11 @@ const controller = require('../controllers/AuthController')
 
 router.post('/login', controller.Login)
 router.post('/register', controller.Register)
+router.get(
+    '/session',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.CheckSession
+  )
 
 module.exports = router
