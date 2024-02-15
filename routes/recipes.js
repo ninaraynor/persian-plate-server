2717ca4713b7e03f01a1f86c0ac9509679d67107
ninaraynor <ninaraynor@gmail.com>
2024-02-15 11:47:47 -0500

    adjust route

--- routes/recipes.js
+++ routes/recipes.js
@@ -10,7 +10,7 @@ router.get("/dishes/:dishId", recipesCtrl.recipesByDish)
 
 router.get("/recipes/:recipeId", recipesCtrl.recipeDetail)
 
-router.get("/recipes/users/:userId", recipesCtrl.recipesByUser)
+router.get("/users/:userId", recipesCtrl.recipesByUser)
 
 //recipes show
 router.get("/:id", recipesCtrl.show)
